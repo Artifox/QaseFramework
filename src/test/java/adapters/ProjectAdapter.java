b8ecb58e43aa8426c424d5d9d2bfa9147ffd6322
Artsiom Koptsiukh <sid93.ramsey@gmail.com>
2021-05-20 14:39:04 +0300

    Fixing Steps to API 3

--- src/test/java/adapters/ProjectAdapter.java
+++ src/test/java/adapters/ProjectAdapter.java
@@ -6,19 +6,19 @@ import models.Project;
 public class ProjectAdapter extends BaseAdapter {
     public static final String URI = "v1/project";
 
-    @Step("Create a new project: 'project.title'")
+    @Step("Create a new project: {project.title}")
     public void post(Project project, boolean expectedResult) {
         String response = post(gson.toJson(project), URI, 200);
         checkStatus(response, expectedResult);
     }
 
-    @Step("Get project: 'project.title'")
+    @Step("Get project: {project.title}")
     public void get(Project project, int status, boolean expectedResult) {
         String response = get(URI + "/" + project.getCode(), status);
         checkStatus(response, expectedResult);
     }
 
-    @Step("Delete project by project code: project.code")
+    @Step("Delete project by project code: {project.code}")
     public void delete(Project project, boolean expectedResult) {
         String response = delete(URI + "/" + project.getCode(), 200);
         checkStatus(response, expectedResult);
